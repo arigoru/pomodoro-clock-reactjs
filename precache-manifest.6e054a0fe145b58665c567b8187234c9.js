e7d135c22f84bc4b9541eb6b8bbd71301f9f7e61
Allin Ihor <ui.ethuil@gmail.com>
2019-02-11 03:15:24 +0200

    Updates

---
+++ precache-manifest.6e054a0fe145b58665c567b8187234c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8665d3e226b24d8a296cb34a3692fd",
+    "url": "/pomodoro-clock-reactjs/static/media/DigitalNumbers-Regular.df8665d3.ttf"
+  },
+  {
+    "revision": "94f068db8e32e6ae643fc8168458eac6",
+    "url": "/pomodoro-clock-reactjs/static/media/DigitalNumbers-Regular.94f068db.svg"
+  },
+  {
+    "revision": "391dd555402f1333805c1222ca6d3bcb",
+    "url": "/pomodoro-clock-reactjs/static/media/DigitalNumbers-Regular.391dd555.eot"
+  },
+  {
+    "revision": "13131940120459697068cf05c7e16bcf",
+    "url": "/pomodoro-clock-reactjs/static/media/DigitalNumbers-Regular.13131940.woff"
+  },
+  {
+    "revision": "d5b337d0a577a54f5805",
+    "url": "/pomodoro-clock-reactjs/static/js/runtime~main.d5b337d0.js"
+  },
+  {
+    "revision": "a4b11558a8b78af9b733",
+    "url": "/pomodoro-clock-reactjs/static/js/main.a4b11558.chunk.js"
+  },
+  {
+    "revision": "9ab444e805cc2359be69",
+    "url": "/pomodoro-clock-reactjs/static/js/1.9ab444e8.chunk.js"
+  },
+  {
+    "revision": "a4b11558a8b78af9b733",
+    "url": "/pomodoro-clock-reactjs/static/css/main.14bf7424.chunk.css"
+  },
+  {
+    "revision": "9ab444e805cc2359be69",
+    "url": "/pomodoro-clock-reactjs/static/css/1.a0f7a307.chunk.css"
+  },
+  {
+    "revision": "b637b7832997c196c08221a7dc126fc6",
+    "url": "/pomodoro-clock-reactjs/index.html"
+  }
+];
